5fbc690126148ab04616f2c5f2f48e1b8f463634
Simon <fctinstean@gmail.com>
2022-04-21 16:21:06 +0200

    exercise bonus

---
+++ PHP/Day 3 - Functions/Exercises/Function_Exercise_BONUS.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+
+We have an array of integers, positive, between 1 and $nbElements (nbElements is the number of elements. You can use this variable).
+
+$array = [5, 2, 3, 6, 7];
+
+Examples :
+
+	Write a function call 'FizzBuzz'.
+	For each number 'n' of the list, we want the following operations:
+		. if the number is divisible by 3 : display 'Fizz'
+		. if the number is divisible by 5 : display 'Buzz'
+		. if the number is divisible by 3 AND by 5 : display 'TicTac'
+		. else : display the number 'n'
+		*/
