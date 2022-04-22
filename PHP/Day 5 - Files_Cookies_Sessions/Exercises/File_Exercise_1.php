da907c9ca49a7f5a7daf881fa35ee74ef4cbe6c1
Simon <fctinstean@gmail.com>
2022-04-22 12:16:50 +0200

    exercises

--- PHP/Day 5 - Files_Cookies_Sessions/Exercises/File_Exercise_1.php
+++ PHP/Day 5 - Files_Cookies_Sessions/Exercises/File_Exercise_1.php
@@ -1,9 +1,9 @@
 <?php
 
-	/*
-		Create a script that saves in the last_access.log file the timestamp of this time (now).
+/*
+	Create a script that saves in the last_access.log file the timestamp of this time (now).
 
-		We will replace the old timestamp.
+	We will replace the old timestamp.
 
-		Choose the right setting to open the file
-	 */
+	Choose the right setting to open the file
+	*/
