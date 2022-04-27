a158277b48636e2aed00b4fd7f8c193fc6676ffd
Simon <fctinstean@gmail.com>
2022-04-27 15:10:10 +0200

    Join

---
+++ DATABASE/Day 3 - Join/join.sql
@@ -0,0 +1,11 @@
+/*
+    JOINING
+
+    Joining allow you to associate/link different tables in the same query.
+    You are making the 'relation' between primary key / foreign key.
+
+*/
+
+SELECT title, name, date_of_birth
+FROM authors a
+LEFT JOIN books b ON a.id = b.author_id
