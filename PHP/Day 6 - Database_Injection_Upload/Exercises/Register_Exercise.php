0473041a514e73d10804e745a54e0cc421e56b85
Simon <fctinstean@gmail.com>
2022-05-02 15:02:53 +0200

    Exercise

---
+++ PHP/Day 6 - Database_Injection_Upload/Exercises/Register_Exercise.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+
+    Exercise :
+
+    In this exercise, you'll have to create the register process.
+
+    - Step 1 :
+        Create a 'register.php' file
+        This file contains a form with those input :
+            * username
+            * email
+            * password
+            * confirm password
+            * submit btn
+
+    - Step 2 :
+        If the form is submitted, you have to check :
+            * username, mail and password must not be empty !
+            * mail must be a valid one
+            * passwords must be the same 
+        
+        You have to clean the inputs using trim and/or htmlspecialchars.
+        Display a different message for each validation/error.
+
+    - Step 3 :
+        If everything is good, insert the new user in the DB !
+
+        You have to encrypt the password !
+
+    */
