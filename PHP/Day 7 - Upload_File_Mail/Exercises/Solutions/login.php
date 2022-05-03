65c32e7811df355f33777d410675690761bbdb2f
Simon <fctinstean@gmail.com>
2022-05-03 16:07:53 +0200

    Login solution

---
+++ PHP/Day 7 - Upload_File_Mail/Exercises/Solutions/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log-in</title>
+</head>
+
+<body>
+    <h1>Log-in</h1>
+
+    <?php
+
+    if (!empty($_POST)) {
+        $email = trim($_POST['email']);
+        $password = $_POST['password'];
+
+        $conn = mysqli_connect('localhost', 'root', '', 'movie_db');
+        $query = "SELECT * FROM users WHERE email = '$email'";
+        $results = mysqli_query($conn, $query);
+        $user = mysqli_fetch_assoc($results);
+
+        // Check if user exists in the DB
+        if ($user != null) {
+            // Compare password from the form and the one from database
+            if (password_verify($password, $user['password']))
+                echo 'Welcome user';
+            else
+                echo 'Password is wrong.';
+        } else {
+            echo 'Email not found. Try with another email.';
+        }
+    }
+
+    ?>
+
+    <form action="" method="post">
+        <input type="text" name="email" placeholder="Your email"><br>
+        <input type="password" name="password" placeholder="Your password"><br>
+        <input type="submit" name="loginBtn" value="Login">
+    </form>
+</body>
+
+</html>
