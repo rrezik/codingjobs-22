3b956bd6e80ca684a332a05e3865fc1dd93646e7
Simon <fctinstean@gmail.com>
2022-05-03 13:53:58 +0200

    Exercise

---
+++ PHP/Day 7 - Upload_File_Mail/Exercises/Login_Exercise.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+    Exercise :
+
+    This exercise is based on the 'Register_Exercise'.
+
+    In this exercise, you'll have to create the login process.
+
+    - Step 1 :
+        Create a 'login.php' file or grab the one from Session_Exercise_1
+    
+    - Step 2 :
+        When the user log in, before saving email in session you must :
+            - Check if the user email exists in the DB.
+            - If it doesn't exists, display error message.
+            - If it exists, check if password matches and display result message.
+            Do not forget to use password_verify.
+
+    */
