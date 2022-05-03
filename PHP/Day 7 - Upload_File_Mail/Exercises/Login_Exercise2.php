799c6725c81446eb66d861ebe24013c46110ba3f
Simon <fctinstean@gmail.com>
2022-05-03 16:14:47 +0200

    Exercise instruction

---
+++ PHP/Day 7 - Upload_File_Mail/Exercises/Login_Exercise2.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+	This exercise is based on the 'login/register' exercise.
+	
+	Now, we have implemented the register/login process into the same project.
+
+	Step 1. Create a 'nav.php' file that contains the two links : register / login
+            Create a link also for the home page (index file)
+            Include this file on all the pages.
+    5Min
+
+    Step 2. Edit 'login.php' page.
+    Once the user is logged-in, create a cookie 'login' to true.
+
+    Step 3.	If the user is loggued-in, hide 'login' & 'register' links and display 'account' instead.
+
+*/
