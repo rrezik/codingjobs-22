fb8797976eb515f8eaa1131722aa6dbd24648be7
Simon <fctinstean@gmail.com>
2022-05-04 15:00:25 +0200

    Update Pagination_Exercise.php

--- PHP/Day 8 - Host_Mail_Pagination/Exercises/Pagination_Exercise.php
+++ PHP/Day 8 - Host_Mail_Pagination/Exercises/Pagination_Exercise.php
@@ -1,12 +1,14 @@
 <?php
 
 /*
-
     Exercise :
 
-    This exercise is based on the 'Movie Exercise'.
+    This exercise is based on the 'Login/Register Exercise'.
 
     In this exercise, you'll have to create the pagination process for the movies list (movie.php page)
+
+    - Step 0 : 
+        Grab/Retrieve 'movies.php' and 'movie.php' in the same project.
 
     - Step 1 :
         Grab the current page number using GET method
